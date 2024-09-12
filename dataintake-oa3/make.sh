dfe9cfbde77c2ceacc21a1bf31f1bea43812f7f7
John Thompson <john@jember.ai>
2024-09-12 20:39:02 +0000

    updating spec and nav

--- dataintake-oa3/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "building redoc API documentation"
-
-# Bundle docs into zero-dependency HTML file
-npx @redocly/cli bundle -o jember-oa3.yaml && \
-npx @redocly/cli build-docs jember-oa3.yaml -o index.html 
